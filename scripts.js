bb2104ba28e9407d0e0e3c0d324b8d7590de0f15
btramel <bradtramel@gmail.com>
2022-01-21 12:26:21 -0600

    submit now creates new div nodes within a grid container

--- scripts.js
+++ scripts.js
@@ -14,17 +14,6 @@ const openModalButtons = document.querySelectorAll('[data-modal-target]');
 const closeModalButtons = document.querySelectorAll('[data-close-button]');
 const overlay = document.getElementById('overlay');
 
-
-function addBook () {
-    let title = document.getElementById('title').value
-    let author = document.getElementById('author').value
-    let pages = document.getElementById('pages').value
-    let read = document.getElementById('verifyread').checked
-    const book = new Book(title, author, pages, read);
-    books.push(book);
-    console.table(books);
-}
-
 openModalButtons.forEach(button => {
     button.addEventListener('click', () => {
         const modal = document.querySelector(button.dataset.modalTarget)
@@ -59,10 +48,45 @@ function closeModal(modal) {
     overlay.classList.remove('active')
 }
 
-const form = document.querySelector('#form');
-const submitBook = document.querySelector('#submit-book');
-submitBook.addEventListener('click', () => {
-    addBook()
-    form.reset()
-    modal.classList.remove('active')
-})
+// var node = document.createElement("LI");                 // Create a <li> node
+// var textnode = document.createTextNode("Water");         // Create a text node
+// node.appendChild(textnode);                              // Append the text to <li>
+// document.getElementById("myList").appendChild(node);     // Append <li> to <ul> with id="myList" 
+
+function addBook () {
+    // get form values
+    let title = document.getElementById('title').value
+    let author = document.getElementById('author').value
+    let pages = document.getElementById('pages').value
+    let read = document.getElementById('verifyread').checked
+
+    // create card nodes
+    const libraryContainer = document.getElementById('libraryContainer')
+    let node = document.createElement('div');
+    node.className = 'card';
+    let textnode = document.createTextNode('new book!')
+
+    // create new book obj
+    const book = new Book(title, author, pages, read);
+    
+    // push new book to array and show it to me
+    books.push(book);
+    console.table(books);
+    
+    // create new card
+    libraryContainer.append(node);
+
+    // add content to card
+    node.appendChild(textnode);
+
+}
+
+const form = document.querySelector('#form')
+const submitBook = document.querySelector('#submit-book')
+
+submitBook.addEventListener("click", (e) => {
+    e.preventDefault();
+    addBook();
+    form.reset();
+    modal.classList.remove('active');
+  });
